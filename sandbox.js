b5b3b35797e9630d2daf6749c1d5c397e74e3c93
MaryyBo <marusya.bodrug@gmail.com>
2024-06-25 12:46:18 +0200

    Generator (задача додому)

--- sandbox.js
+++ sandbox.js
@@ -1,38 +1,38 @@
 // Функції - генератори
 
 function* myGenerator() {
-    ///
-    yield 1;
-    ///
-    yield 2;
-    ///
-    yield 3;
-  }
-  
-  const gen = myGenerator();
-  
-  
-  
-  function* mySecondGenerator(start, end) {
-    for(let i = start; i <= end; i++) {
-      yield i;
-    }
-  }
-  
-  const gen2 = mySecondGenerator(1, 5);
-  
-  function* myThirdGenerator() {
-    let value = yield;
-    console.log(value);
-  }
-  
-  const gen3 = myThirdGenerator();
-  
-  gen3.next(); // запускає генератор
-  
-  gen3.next(); // передає значення у генератор
+  ///
+  yield 1;
+  ///
+  yield 2;
+  ///
+  yield 3;
+}
 
-  
+const gen = myGenerator();
+
+
+
+function* mySecondGenerator(start, end) {
+  for (let i = start; i <= end; i++) {
+    yield i;
+  }
+}
+
+const gen2 = mySecondGenerator(1, 5);
+
+function* myThirdGenerator() {
+  let value = yield;
+  console.log(value);
+}
+
+const gen3 = myThirdGenerator();
+
+gen3.next(); // запускає генератор
+
+gen3.next(); // передає значення у генератор
+
+
 // Ітератори 
 
 /*
@@ -68,34 +68,49 @@ iterator.next(); // Отримати наступний елемент
 // customIterator
 
 const myIterator = {
-    data: [1, 2, 3, 4, 5],
-    currentIndex: 0,
-    next() {
-      if(this.currentIndex < this.data.length) {
-        return {
-          value: this.data[this.currentIndex++],
-          done: false
-        }
-      } else {
-        return {
-          value: undefined,
-          done: true
-        }
+  data: [1, 2, 3, 4, 5],
+  currentIndex: 0,
+  next() {
+    if (this.currentIndex < this.data.length) {
+      return {
+        value: this.data[this.currentIndex++],
+        done: false
+      }
+    } else {
+      return {
+        value: undefined,
+        done: true
       }
     }
   }
-  
-  
-  
-  
-  
-  // ДЗ
-  /* 
-  
-  Написати функцію-генератор, яка генерує числа від 0 до 100.
-  З кожним викликом число інкрементується на 1.
-  
-  За допомогою написаного генератора, потрібно знайти суму чисел від 0 до 100.
-  (5050)
-  
-  */
+}
+
+
+
+
+
+// ДЗ
+/* 
+ 
+Написати функцію-генератор, яка генерує числа від 0 до 100.
+З кожним викликом число інкрементується на 1.
+ 
+За допомогою написаного генератора, потрібно знайти суму чисел від 0 до 100.
+(5050)
+ 
+*/
+
+function* numberGenerator(start, end) {
+  for (let i = start; i <= end; i++) {
+    yield i;
+  }
+}
+
+const iter = numberGenerator(0, 100);
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+  const {value} = iter.next();
+  sum += value;
+}
+
+// 5050
