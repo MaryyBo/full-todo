26a6254cf2e24575827528c2fe9909bfd201f17c
MaryyBo <marusya.bodrug@gmail.com>
2024-07-04 19:29:49 +0200

    devTools enhancer

--- client/src/store.js
+++ client/src/store.js
@@ -2,10 +2,11 @@ import { legacy_createStore as createStore, applyMiddleware } from 'redux'
 import reducer from './reducer';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas';
+import { composeWithDevTools } from '@redux-devtools/extension'
 
 const sagaMiddleware = createSagaMiddleware();
 
-const enhancer = applyMiddleware(sagaMiddleware);// повертається покращувач для store
+const enhancer = composeWithDevTools(applyMiddleware(sagaMiddleware));// повертається покращувач для store
 
 const store = createStore(reducer, enhancer); // ми прикрутили middleware (enhancer) до нашої store
 
